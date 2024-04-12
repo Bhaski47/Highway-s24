fb831caadda8a39f56e416a102f72bc11ab5eb39
Bhaski47 <bhaskarg2003@gmail.com>
2024-04-12 16:11:47 +0530

    Added Video

--- client/src/utils/Video.tsx
+++ client/src/utils/Video.tsx
@@ -1,14 +1,14 @@
 import styles from '../styles/pages/about.module.css'
 import React from 'react';
-// import vid4 from '../assets/Highways24.mp4'
-import ark1 from '../assets/images/ark1.jpg'
+import vid4 from '../assets/Highways24.mp4'
+// import ark1 from '../assets/images/ark1.jpg'
 const Video: React.FC = () => {
   return (
     <div>
-      {/* <video controls={false} autoPlay={true} className={styles.img} loop={true}>
+      <video controls={false} autoPlay={true} className={styles.img} loop={true}>
         <source src={vid4} type="video/mp4" />
-      </video> */}
-      <img src={ark1} alt="vid" className={styles.img}/>
+      </video>
+      {/* <img src={ark1} alt="vid" className={styles.img}/> */}
     </div>
   );
 }
