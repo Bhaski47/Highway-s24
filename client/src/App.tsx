314d937f830ed01d02aaf698ad7fe49b20185453
Bhaski47 <bhaskarg2003@gmail.com>
2024-04-16 22:55:21 +0530

    test5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,103 +6,110 @@ import Footer from './components/common/Footer';
 import Sponsors from './page/Sponsors';
 import Location from './page/Location';
 // import Events from './page/Events';
+import ReactGA from 'react-ga4'
 import EventSection from './page/EventSection';
 import EventInfo from './page/EventInfo';
 import Schedule from './page/Schedule';
 import TermsAndConditions from './page/Terms';
 import GetPasses from './page/GetPasses';
 import BuyPass from './page/BuyPass';
+import { useEffect } from "react";
+useEffect(() => {
+  ReactGA.initialize(`${process.env.REACT_API_BASE_TRACKING_ID}`);
+  ReactGA.send({ hitType: "pageview", page: "/landingpage", title: "Landing Page" });
+  console.log(process.env.REACT_API_BASE_TRACKING_ID);
+}, [])
 const router = createBrowserRouter(
   [
     {
       path: "/",
-      element:<Home/>,
+      element: <Home />,
     },
     {
-      path:"/about",
+      path: "/about",
       element:
-      <>
-        <About />
-        <Footer />
-      </>
-  },
-  {
-    path: "/sponsors",
-    element: (
-      <>
-        <Sponsors />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/location",
-    element: (
-      <>
-        <Location />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/events",
-    element: (
-      <>
-        <EventSection />
-        <Footer />
-      </>
-    ),
-  },
-  // {
-  //   path: "/events/:events",
-  //   element: (
-  //     <>
-  //       <Events />
-  //       <Footer />
-  //     </>
-  //   ),
-  // },
-  {
-    path: "/events/:eventId",
-    element: <EventInfo />,
-  },
-  {
-    path: "/schedule",
-    element: (
-      <>
-        <Schedule />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/getPasses",
-    element: (
-      <>
-        <GetPasses />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/buyPass/:pass",
-    element: (
-      <>
-        <BuyPass />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/termsandconditions",
-    element: (
-      <>
-        <TermsAndConditions />
-        <Footer />
-      </>
-    ),
-  },
-]);
+        <>
+          <About />
+          <Footer />
+        </>
+    },
+    {
+      path: "/sponsors",
+      element: (
+        <>
+          <Sponsors />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/location",
+      element: (
+        <>
+          <Location />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/events",
+      element: (
+        <>
+          <EventSection />
+          <Footer />
+        </>
+      ),
+    },
+    // {
+    //   path: "/events/:events",
+    //   element: (
+    //     <>
+    //       <Events />
+    //       <Footer />
+    //     </>
+    //   ),
+    // },
+    {
+      path: "/events/:eventId",
+      element: <EventInfo />,
+    },
+    {
+      path: "/schedule",
+      element: (
+        <>
+          <Schedule />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/getPasses",
+      element: (
+        <>
+          <GetPasses />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/buyPass/:pass",
+      element: (
+        <>
+          <BuyPass />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/termsandconditions",
+      element: (
+        <>
+          <TermsAndConditions />
+          <Footer />
+        </>
+      ),
+    },
+  ]);
 function App() {
   return (
     <>
